026dfb42b9bfd772b29456ee1692b63f19f7148d
Jacob Holtom <webmasterlegobrains@gmail.com>
2013-06-16 12:47:02 -0400

    new kernel and header

--- kernel.c
+++ kernel.c
@@ -3,121 +3,81 @@
 #endif
 #include <stddef.h>
 #include <stdint.h>
-
 #include < system.h >
 
 #if defined(__linux__)
 #error "Need a crosscompiler. probably should get one. Unless you love errors!"
 #endif
- 
-/* Hardware text mode color constants. */
-enum vga_color
+
+void *memcpy(void *dest, const void *src, size_t count)
 {
-	COLOR_BLACK = 0,
-	COLOR_BLUE = 1,
-	COLOR_GREEN = 2,
-	COLOR_CYAN = 3,
-	COLOR_RED = 4,
-	COLOR_MAGENTA = 5,
-	COLOR_BROWN = 6,
-	COLOR_LIGHT_GREY = 7,
-	COLOR_DARK_GREY = 8,
-	COLOR_LIGHT_BLUE = 9,
-	COLOR_LIGHT_GREEN = 10,
-	COLOR_LIGHT_CYAN = 11,
-	COLOR_LIGHT_RED = 12,
-	COLOR_LIGHT_MAGENTA = 13,
-	COLOR_LIGHT_BROWN = 14,
-	COLOR_WHITE = 15,
-};
- 
-uint8_t make_color(enum vga_color fg, enum vga_color bg)
-{
-	return fg | bg << 4;
+        const char *sp = (const char *)src;
+        char *dp = (char *)dest;
+        for(; count != 0; count--) *dp++ = *sp++;
+        return dest;
 }
- 
-uint16_t make_vgaentry(char c, uint8_t color)
+
+void *memset(void *dest, char val, size_t count)
 {
-	uint16_t c16 = c;
-	uint16_t color16 = color;
-	return c16 | color16 << 8;
+        char *temp = (char *)dest;
+        for( ; count != 0; count--) *temp++ = val;
+        return dest;
 }
- 
-size_t strlen(const char* str)
+
+unsigned short *memsetw(unsigned short *dest, unsigned short val, size_t count)
 {
-	size_t ret = 0;
-	while ( str[ret] != 0 )
-		ret++;
-	return ret;
+        unsigned short *temp = (unsigned short *)dest;
+        for( ; count != 0; count--) *temp++ = val;
+        return dest;
 }
- 
-static const size_t VGA_WIDTH = 80;
-static const size_t VGA_HEIGHT = 24;
- 
-size_t terminal_row;
-size_t terminal_column;
-uint8_t terminal_color;
-uint16_t* terminal_buffer;
- 
-void terminal_initialize()
+
+size_t strlen(const char *str)
 {
-	terminal_row = 0;
-	terminal_column = 0;
-	terminal_color = make_color(COLOR_LIGHT_GREY, COLOR_BLACK);
-	terminal_buffer = (uint16_t*) 0xB8000;
-	for ( size_t y = 0; y < VGA_HEIGHT; y++ )
-	{
-		for ( size_t x = 0; x < VGA_WIDTH; x++ )
-		{
-			const size_t index = y * VGA_WIDTH + x;
-			terminal_buffer[index] = make_vgaentry(' ', terminal_color);
-		}
-	}
+        size_t retval;
+        for(retval = 0; *str != '\0'; str++) retval++;
+        return retval;
 }
- 
-void terminal_setcolor(uint8_t color)
+
+unsigned char inportb (unsigned short _port)
 {
-	terminal_color = color;
+        unsigned char rv;
+        __asm__ __volatile__ ("inb %1, %0" : "=a" (rv) : "dN" (_port));
+        return rv;
 }
- 
-void terminal_putentryat(char c, uint8_t color, size_t x, size_t y)
+
+void outportb (unsigned short _port, unsigned char _data)
 {
-	const size_t index = y * VGA_WIDTH + x;
-	terminal_buffer[index] = make_vgaentry(c, color);
+        __asm__ __volatile__ ("outb %1, %0" : : "dN" (_port), "a" (_data));
 }
- 
-void terminal_putchar(char c)
-{
-	if(c == "\n")
-    {
-    terminal_row++;
-    terminal_column = 0;
-    }
-    terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
-	if ( ++terminal_column == VGA_WIDTH )
-	{
-		terminal_column = 0;
-		if ( ++terminal_row == VGA_HEIGHT )
-		{
-			terminal_row = 0;
-		}
-	}
-}
- 
-void terminal_writestring(const char* data)
-{
-	size_t datalen = strlen(data);
-	for ( size_t i = 0; i < datalen; i++ )
-		terminal_putchar(data[i]);
-}
- 
+
 #if defined(__cplusplus)
 extern "C" /* Use C linkage for kernel_main. */
 #endif
 void kernel_main()
 {
-	terminal_initialize();
-	/*implement \n since now we get vga-specific newline char */
-    terminal_writestring("Hello, kernel World!\n");
-    terminal_writestring("> ");
+        gdt_install();
+        idt_install();
+        isrs_install();
+        irq_install();
+        init_video();
+        timer_install();
+        keyboard_install();
+
+        __asm__ __volatile__ ("sti");
+
+        puts("XitoxOS Loaded\n");
+
+        for(;;);
 }
+
+
+
+
+
+
+
+
+
+
+
+
