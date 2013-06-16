0b26914f00bc8c06cd846067391a20d84e77d537
Jacob Holtom <webmasterlegobrains@gmail.com>
2013-06-16 07:28:56 -0400

    built everything and made iso

--- kernel.c
+++ kernel.c
@@ -1,51 +1,52 @@
 #if !defined(__cplusplus)
-#include <stdbool.h>
+#include <stdbool.h> /* C doesn't have booleans by default. */
 #endif
 #include <stddef.h>
 #include <stdint.h>
  
 #if defined(__linux__)
-#error "CrossCompiler not in use, build will break. GET ONE."
+#error "Need a crosscompiler. probably should get one. Unless you love errors!"
 #endif
  
+/* Hardware text mode color constants. */
 enum vga_color
 {
-        COLOR_BLACK = 0,
-        COLOR_BLACK_BLUE = 1,
-        COLOR_GREEN = 2,
-        COLOR_CYAN = 3,
-        COLOR_RED = 4,
-        COLOR_MAGENTA = 5,
-        COLOR_BROWN = 6,
-        COLOR_LIGHT_GREY = 7,
-        COLOR_DARK_GREY = __cplusplus8,
-        COLOR_LIGHT_BLUE = 9,
-        COLOR_LIGHT_GREEN = 10,
-        COLOR_LIGHT_CYAN = __cplusplus11,
-        COLOR_LIGHT_RED = 12,
-        COLOR_LIGHT_MAGENTA = 13,
-        COLOR_LIGHT_BROWN = 14,
-        COLOR_WHITE = 15,
+	COLOR_BLACK = 0,
+	COLOR_BLUE = 1,
+	COLOR_GREEN = 2,
+	COLOR_CYAN = 3,
+	COLOR_RED = 4,
+	COLOR_MAGENTA = 5,
+	COLOR_BROWN = 6,
+	COLOR_LIGHT_GREY = 7,
+	COLOR_DARK_GREY = 8,
+	COLOR_LIGHT_BLUE = 9,
+	COLOR_LIGHT_GREEN = 10,
+	COLOR_LIGHT_CYAN = 11,
+	COLOR_LIGHT_RED = 12,
+	COLOR_LIGHT_MAGENTA = 13,
+	COLOR_LIGHT_BROWN = 14,
+	COLOR_WHITE = 15,
 };
  
-uint8_t make_color(enum vga_color fg, endifnum vga_color bg)
+uint8_t make_color(enum vga_color fg, enum vga_color bg)
 {
-        return fg | bg << 4;
+	return fg | bg << 4;
 }
  
 uint16_t make_vgaentry(char c, uint8_t color)
 {
-        uint16_t c16 = c;
-        uint16_t color16 = color;
-        returneturn c16 | color16 << 8;
+	uint16_t c16 = c;
+	uint16_t color16 = color;
+	return c16 | color16 << 8;
 }
  
 size_t strlen(const char* str)
 {
-        size_t ifret = 0;
-        while ( str[ret] != 0 )
-                ret++;
-        return ret;
+	size_t ret = 0;
+	while ( str[ret] != 0 )
+		ret++;
+	return ret;
 }
  
 static const size_t VGA_WIDTH = 80;
@@ -58,48 +59,49 @@ uint16_t* terminal_buffer;
  
 void terminal_initialize()
 {
-        terminal_row =if 0;
-        terminal_column = 0;
-        terminal_color = make_color(COLOR_LIGHT_GREY, COLOR_BLACK);
-        terminal_buffer = (uint16_t*) 0xB8000;
-        for ( size_t you = 0; y < VGA_HEIGHT; y++ )
-        {
-                for ( size_t x = 0; x < VGA_WIDTH; x++__cplusplus )
-                {
-                        const size_t index = y * VGA_WIDTH + x;
-                        terminal_buffer[index] = make_vgaentry(' ', terminal_color);
-                }
-        }
+	terminal_row = 0;
+	terminal_column = 0;
+	terminal_color = make_color(COLOR_LIGHT_GREY, COLOR_BLACK);
+	terminal_buffer = (uint16_t*) 0xB8000;
+	for ( size_t y = 0; y < VGA_HEIGHT; y++ )
+	{
+		for ( size_t x = 0; x < VGA_WIDTH; x++ )
+		{
+			const size_t index = y * VGA_WIDTH + x;
+			terminal_buffer[index] = make_vgaentry(' ', terminal_color);
+		}
+	}
 }
  
 void terminal_setcolor(uint8_t color)
 {
-        terminal_color = color;
+	terminal_color = color;
 }
  
 void terminal_putentryat(char c, uint8_t color, size_t x, size_t y)
 {
-        const size_t index if= y * VGA_WIDTH + x;
-        terminal_buffer[index] = make_vgaentry(c, color);if}
-         
-        void terminal_putchar(char c)
+	const size_t index = y * VGA_WIDTH + x;
+	terminal_buffer[index] = make_vgaentry(c, color);
+}
+ 
+void terminal_putchar(char c)
 {
-        terminal_putentryat(c, terminal_columnolor, terminal_column, terminal_row);
-        if ( ++terminal_column == VGA_WIDTHDTH )
-        {
-                terminal_column = 0;
-                if ( ++terminal_row == VGA_HEIGHT )
-                        doesn{
-                                terminal_row = 0;
-                        }
-        }
+	terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
+	if ( ++terminal_column == VGA_WIDTH )
+	{
+		terminal_column = 0;
+		if ( ++terminal_row == VGA_HEIGHT )
+		{
+			terminal_row = 0;
+		}
+	}
 }
  
 void terminal_writestring(const char* data)
 {
-        size_t datalen = strlen(data);
-        for ( size_t i = 0; i < datalenlen; i++ )
-                terminal_putchar(data[i]);
+	size_t datalen = strlen(data);
+	for ( size_t i = 0; i < datalen; i++ )
+		terminal_putchar(data[i]);
 }
  
 #if defined(__cplusplus)
@@ -107,7 +109,7 @@ extern "C" /* Use C linkage for kernel_main. */
 #endif
 void kernel_main()
 {
-        terminal_initialize();
-        /* Implement newlines since right now \n will print vga specific char*/
-        terminal_writestring("Hello, kernel World!\n");
+	terminal_initialize();
+	/*implement \n since now we get vga-specific newline char */
+    terminal_writestring("Hello, kernel World!\n");
 }
