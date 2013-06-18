765ede0a236c33fef661a15da1fa4ee58bdf8fcb
Jacob Holtom <webmasterlegobrains@gmail.com>
2013-06-18 18:23:41 -0400

    restored old timer

--- kernel/timer.c
+++ kernel/timer.c
@@ -2,30 +2,23 @@
 #include <isr.h>
 #include <monitor.h>
 
-static void timer_callback(registers_t *regs)
+u32int tick = 0;
+
+static void timer_callback(registers_t regs)
 {
     tick++;
-    switch_task();
+    monitor_write("Tick: ");
+    monitor_write_dec(tick);
+    monitor_write("\n");
 }
 
 void init_timer(u32int frequency)
 {
-    // Firstly, register our timer callback.
     register_interrupt_handler(IRQ0, &timer_callback);
-
-    // The value we send to the PIT is the value to divide it's input clock
-    // by, to get our required frequency. Important to note is
-    // that the divisor must be small enough to fit into 16-bits.
-    u32int divisor = 1193180 / frequency; // 1193180 Hz
-
-    // Send the command byte.
+    u32int divisor = 1193180 / frequency;
     outb(0x43, 0x36);
-
-    // Divisor has to be sent byte-wise, so split here into upper/lower bytes.
     u8int l = (u8int)(divisor & 0xFF);
     u8int h = (u8int)( (divisor>>8) & 0xFF );
-
-    // Send the frequency divisor.
     outb(0x40, l);
     outb(0x40, h);
 }
