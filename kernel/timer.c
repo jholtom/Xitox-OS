3f2292df2bb5e5bc25fbf8ae4bea913cde71923b
Jacob Holtom <webmasterlegobrains@gmail.com>
2013-06-18 18:48:04 -0400

    timer fix

--- kernel/timer.c
+++ kernel/timer.c
@@ -4,21 +4,22 @@
 
 u32int tick = 0;
 
-static void timer_callback(registers_t regs)
+void timer_callback(registers_t regs)
 {
     tick++;
-    monitor_write("Tick: ");
-    monitor_write_dec(tick);
-    monitor_write("\n");
+    if(tick % 18 == 0)
+    {
+      monitor_write("another second bit the dust\n"); 
+    }
 }
 
 void init_timer(u32int frequency)
 {
     register_interrupt_handler(IRQ0, &timer_callback);
-    u32int divisor = 1193180 / frequency;
+    /*u32int divisor = 1193180 / frequency;
     outb(0x43, 0x36);
     u8int l = (u8int)(divisor & 0xFF);
     u8int h = (u8int)( (divisor>>8) & 0xFF );
     outb(0x40, l);
-    outb(0x40, h);
-}
+    outb(0x40, h);*/
+    }
